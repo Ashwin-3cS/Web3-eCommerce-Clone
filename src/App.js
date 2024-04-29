d29f466fa23b6ec94e2d35a5cf641ff20b92e445
Ashwin-3cS <flashashwin2004@gmail.com>
2024-04-29 15:49:30 +0530

    facing an issue while trying to connect to metamask

--- src/App.js
+++ src/App.js
@@ -14,10 +14,32 @@ import config from './config.json'
 
 function App() {
 
+  const [account , setAccount] = useState(null);
+
+  const loadBlockchainData =  async () =>{
+    const accounts = window.ethereum.request({method:'eth_requestAccounts'});
+    const account_var =  ethers.utils.getAddress(accounts[0])
+    setAccount(account_var)
+
+
+    try {
+      console.log(account)
+    } catch (error) {
+      console.error(error.message)
+      
+    }
+  }
+  useEffect(()=>{
+    loadBlockchainData();
+  },[])
+
   return (
     <div>
 
+      <Navigation account={account} setAccount={setAccount} />
+
       <h2>Welcome to Dappazon </h2>
+      <p>{account}</p>
 
     </div>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,8 +2,24 @@ import { ethers } from 'ethers';
 
 const Navigation = ({ account, setAccount }) => {
 
+
     return (
         <nav>
+        <div className='nav__brand'>
+            <h1>Dappazon</h1>
+        </div>
+        <input
+            type="text"
+            className='nav__search'
+        />
+
+        <button
+            type="button"
+            className='nav__connect'
+        >
+            {account}
+            
+        </button>
 
         </nav>
     );
